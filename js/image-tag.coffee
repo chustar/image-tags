8b755b392402a823d71300ab2da53657a2ec3115
Chuma Nnaji <cnnaji2003@gmail.com>
2012-03-17 22:38:02 -0700

    Finished project

--- js/image-tag.coffee
+++ js/image-tag.coffee
@@ -9,14 +9,46 @@ $.fn.image_tag = (options) ->
 placeMarker = (e) ->
 	parent = $(this).parent()
 	marker = new Marker(parent, e.pageX, e.pageY)
-	parent.on('mouseup', (e) ->
-		text = new Text(this, e.pageX, e.pageY, marker)
-		$(this).off('mouseup')
+	line = new Line(parent, e.pageX + 8, e.pageY + 3)
+	
+	parent.on('mousemove', (e) ->
+		css = {}
+		css.width = Math.abs(e.pageX - line.x + 1)
+		if(e.pageX + 8 < line.x)
+			css.left = e.pageX
+			css.width = Math.abs(e.pageX - line.x) - 7
+
+		line.line.css(css)
 	)
+
+	parent.on('mouseup', (e) ->
+		text = {}
+		if(e.pageX < marker.x)
+			text = new Text(this, e.pageX - 100 - 9, marker.y, marker, line)
+		else
+			text = new Text(this, e.pageX, marker.y, marker, line)
+
+		$(this).off('mouseup')
+		$(this).off('mousemove')
+	)
+
+class Line
+	constructor: (div, x, y) ->
+		this.line = $('<div class="line" id="line-' + x + '+' + y + '"></div>')
+		this.x = x
+		this.y = y
+		this.line.css({
+			left: x + 'px',
+			top: y + 'px'
+		})
+		
+		div.append(this.line)
 
 class Marker
 	constructor: (div, x, y) ->
 		this.marker = $('<div class="marker" id="marker-' + x + '+' + y + '"></div>')
+		this.x = x
+		this.y = y
 		this.marker.css({
 			left: x + 'px',
 			top: y + 'px'
@@ -25,7 +57,8 @@ class Marker
 		div.append(this.marker)
 
 class Text
-	constructor: (div, x, y, marker) ->
+	constructor: (div, x, y, marker, line) ->
+		this.line = line
 		this.marker = marker
 		this.text = $('<p class="text" id="text-' + x + '+' + y + '"></p>')
 		this.text.css({
@@ -36,14 +69,16 @@ class Text
 		$(div).append(this.text)
 		this.text.prop('contentEditable', true)
 		this.text.on('keypress', (e) ->
+			
 			if(e.keyCode == 13)
 				$(this).prop('contentEditable', false)
 				$(this).blur()
 		)
+
 		this.text.on('blur', (e) ->
-			console.log($(this).text())
 			if($.trim($(this).text()) == '')
 				$(this).remove()
+				line.line.remove()
 				marker.marker.remove()
 		)
 		this.text.focus()
--- js/image-tag.js
+++ js/image-tag.js
@@ -1,5 +1,5 @@
 (function() {
-  var $, Marker, Text, placeMarker;
+  var $, Line, Marker, Text, placeMarker;
 
   $ = jQuery;
 
@@ -10,20 +10,56 @@
   };
 
   placeMarker = function(e) {
-    var marker, parent;
+    var line, marker, parent;
     parent = $(this).parent();
     marker = new Marker(parent, e.pageX, e.pageY);
+    line = new Line(parent, e.pageX + 8, e.pageY + 3);
+    parent.on('mousemove', function(e) {
+      var css;
+      css = {};
+      css.width = Math.abs(e.pageX - line.x + 1);
+      if (e.pageX + 8 < line.x) {
+        css.left = e.pageX;
+        css.width = Math.abs(e.pageX - line.x) - 7;
+      }
+      return line.line.css(css);
+    });
     return parent.on('mouseup', function(e) {
       var text;
-      text = new Text(this, e.pageX, e.pageY, marker);
-      return $(this).off('mouseup');
+      text = {};
+      if (e.pageX < marker.x) {
+        text = new Text(this, e.pageX - 100 - 9, marker.y, marker, line);
+      } else {
+        text = new Text(this, e.pageX, marker.y, marker, line);
+      }
+      $(this).off('mouseup');
+      return $(this).off('mousemove');
     });
   };
+
+  Line = (function() {
+
+    function Line(div, x, y) {
+      this.line = $('<div class="line" id="line-' + x + '+' + y + '"></div>');
+      this.x = x;
+      this.y = y;
+      this.line.css({
+        left: x + 'px',
+        top: y + 'px'
+      });
+      div.append(this.line);
+    }
+
+    return Line;
+
+  })();
 
   Marker = (function() {
 
     function Marker(div, x, y) {
       this.marker = $('<div class="marker" id="marker-' + x + '+' + y + '"></div>');
+      this.x = x;
+      this.y = y;
       this.marker.css({
         left: x + 'px',
         top: y + 'px'
@@ -37,7 +73,8 @@
 
   Text = (function() {
 
-    function Text(div, x, y, marker) {
+    function Text(div, x, y, marker, line) {
+      this.line = line;
       this.marker = marker;
       this.text = $('<p class="text" id="text-' + x + '+' + y + '"></p>');
       this.text.css({
@@ -53,9 +90,9 @@
         }
       });
       this.text.on('blur', function(e) {
-        console.log($(this).text());
         if ($.trim($(this).text()) === '') {
           $(this).remove();
+          line.line.remove();
           return marker.marker.remove();
         }
       });
